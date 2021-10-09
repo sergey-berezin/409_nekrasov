9386c69cb1758c63cd9cb83fab73f46a2afd1211
Nikolay Nekrasov <nikolaynekrasov1999@gmail.com>
2021-10-09 08:47:14 +0300

    Fixes

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,31 +2,33 @@
 using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using YOLOv4MLNet;
 
 namespace ConsoleUI
 {
     public class Program
     {
+        static CancellationTokenSource cts;
         private static void Handler(object sender, ConsoleCancelEventArgs args)
         {
-            Environment.Exit(0);
+            args.Cancel = true;
+            cts.Cancel();
         }
         static async Task Main()
         {
-
+            cts = new CancellationTokenSource();
+            Console.CancelKeyPress += Handler;
+            CancellationToken ct = cts.Token;
             Console.WriteLine("Для отмены выполнения нажмите Ctrl+с на английской раскладке.");
             Perception perc = new Perception(@"D:\MachineLearning\yolov4.onnx");
             DirectoryInfo di = new DirectoryInfo(@"D:\MachineLearning\Input");
-            FileInfo[] imgs = di.GetFiles();
-            ConsoleView cv = new ConsoleView(imgs.Length);
-            CancellationTokenSource cts = new CancellationTokenSource();
-            CancellationToken ct = cts.Token;
-            Console.CancelKeyPress += Handler;
-            foreach (FileInfo flinf in imgs)
-            {
-                await perc.StartPerception(cv.Model, flinf.FullName, ct);
-            }
+            ConsoleView cv = new ConsoleView(di.GetFiles().Length);
+            IEnumerable<FileInfo> fi = new List<FileInfo>(di.GetFiles());
+            var tasks = fi.Select(flinf => perc.StartPerception(cv.Model, flinf.FullName, ct));
+            await Task.WhenAll(tasks);
+            cts.Dispose();
         }
     }
 }
