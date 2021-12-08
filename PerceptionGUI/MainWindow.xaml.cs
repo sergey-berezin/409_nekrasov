36269ee825be403acde9ab52768bc7f9a75a18fc
Nikolay Nekrasov <nikolaynekrasov1999@gmail.com>
2021-12-09 01:55:38 +0300

    Fourth Lab, fixed

--- PerceptionGUI/MainWindow.xaml.cs
+++ PerceptionGUI/MainWindow.xaml.cs
@@ -31,22 +31,22 @@ namespace PerceptionGUI
         private async void GetStartValuesAsync()
         {
             string response;
-            //try
-            //{
+            try
+            {
                 response = await client.GetStringAsync("https://localhost:44319/api/images");
                 var res = JsonConvert.DeserializeObject<List<ImgDescription>>(response);
-                ListViewImagesInProcess.ItemsSource = view.ImagesInProcess;
+                RefreshDataButtonClick(this, new RoutedEventArgs());
                 ImagesDBListBox.ItemsSource = res;
                 for (int i = 0; i < res.Count; ++i)
                 {
                     view.Model.Add(res[i], res[i].ImgName);
                 }
                 DataContext = view;
-            //} catch
-            //{
-            //    MessageBox.Show("Сервис недоступен");
-            //}
-        }
+            } catch
+            {
+                MessageBox.Show("Сервис недоступен");
+            }
+}
         private void ChooseFolderButtonClick(object sender, RoutedEventArgs e)
         {
             VistaOpenFileDialog dial = new VistaOpenFileDialog();
@@ -58,57 +58,59 @@ namespace PerceptionGUI
         }
         private async void StartPerceptionButtonClick(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
+            try
+            {
                 System.Drawing.Image image = System.Drawing.Image.FromFile(FolderPathTextBox.Text);
                 System.IO.MemoryStream memoryStream = new System.IO.MemoryStream();
                 image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
                 MultipartFormDataContent content = new MultipartFormDataContent();
                 content.Add(new StreamContent(memoryStream), "file", FolderPathTextBox.Text);
                 await client.PostAsync("https://localhost:44319/api/images", content);
-            //} catch
-            //{
-            //    MessageBox.Show("Сервис недоступен");
-            //}
-        }
+            } catch
+            {
+                MessageBox.Show("Сервис недоступен");
+            }
+}
         private async void RefreshDataButtonClick(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
+            try
+            {
                 var response = await client.GetStringAsync("https://localhost:44319/api/images");
                 var res = JsonConvert.DeserializeObject<List<ImgDescription>>(response);
                 ImagesDBListBox.ItemsSource = res;
                 WPFView newView = new WPFView();
                 for (int i = 0; i < res.Count; ++i)
                 {
-                    if (!view.Model.ImagesInProcess.Contains(res[i].ImgName))
+                    newView.Model.Add(res[i], res[i].ImgName);
+                    using (var bitmap = new Bitmap(System.Drawing.Image.FromFile(res[i].ImgName)))
                     {
-                        view.Model.Add(res[i], res[i].ImgName);
-                        using (var bitmap = new Bitmap(System.Drawing.Image.FromFile(res[i].ImgName)))
-                        {
                         foreach (var obj in res[i].Objs)
+                        {
+                            using (var g = Graphics.FromImage(bitmap))
                             {
-                                using (var g = Graphics.FromImage(bitmap))
+                                g.DrawRectangle(Pens.Red, (int)obj.x, (int)obj.y, (int)obj.width, (int)obj.height);
+                                using (var brushes = new SolidBrush(Color.FromArgb(50, Color.Red)))
                                 {
-                                    g.DrawRectangle(Pens.Red, (int)obj.x, (int)obj.y, (int)obj.width, (int)obj.height);
-                                    using (var brushes = new SolidBrush(Color.FromArgb(50, Color.Red)))
-                                    {
-                                        g.FillRectangle(brushes, (int)obj.x, (int)obj.y, (int)obj.width, (int)obj.height);
-                                    }
-                                    g.DrawString(obj.Cls, new Font("Arial", 12), Brushes.Blue, new PointF((int)obj.x, (int)obj.y));
+                                    g.FillRectangle(brushes, (int)obj.x, (int)obj.y, (int)obj.width, (int)obj.height);
                                 }
+                                g.DrawString(obj.Cls, new Font("Arial", 12), Brushes.Blue, new PointF((int)obj.x, (int)obj.y));
                             }
-                            bitmap.Save(res[i].ImgName + "Result" + ".jpg");
-                            view.Model.InsertImage(0, res[i].ImgName);
                         }
+                        if (!view.Model.ImagesInProcess.Contains(res[i].ImgName + "Result" + ".jpg"))
+                        {
+                            bitmap.Save(res[i].ImgName + "Result" + ".jpg");
+                        }
+                        newView.Model.InsertImage(0, res[i].ImgName + "Result" + ".jpg");
                     }
                 }
-                //view = newView;
+                view = newView;
                 DataContext = view;
-            //} catch
-            //{
-            //    MessageBox.Show("Сервис недоступен");
-            //}
+                ListViewImagesInProcess.ItemsSource = view.Model.ImagesInProcess;
+            }
+            catch
+            {
+                MessageBox.Show("Сервис недоступен");
+            }
         }
         private void ClassCountSelectionChanged(object sender, SelectionChangedEventArgs args)
         {
